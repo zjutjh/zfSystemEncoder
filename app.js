41467066071c8a9c95bbaeb9703a2753586c244d
阿瓦隆 <awalong@wacai.com>
2018-03-13 20:05:29 +0800

    format

--- app.js
+++ app.js
@@ -1,36 +1,34 @@
-const Koa=require('koa');
-const app=new Koa();
+const Koa = require('koa')
+const Router = require('koa-router')
+
+const app = new Koa()
 const {
-    hex2b64,
-    b64tohex
-}=require('./rsa/base64');
-const RSAKey=require('./rsa/rsa');
+  hex2b64,
+  b64tohex
+} = require('./rsa/base64')
+
+const RSAKey = require('./rsa/rsa')
 app.use(async (ctx)=>{
-    const rsaKey=new RSAKey();
+    const rsaKey = new RSAKey()
     //获得url
-    let url=ctx.url;
-    let request=ctx.request;
-    let req_query=request.query;
-    // let req_queryString=request.querystring;
-    // let ctx_query=ctx.query;
-    // let ctx_queryString=ctx.querystring;
-    let enPassword=null;
-    let password=req_query.password;
-    let exponent=req_query.exponent;
-    let modules=req_query.modules;
-    if(password&&exponent&&modules){
-        rsaKey.setPublic(b64tohex(modules),b64tohex(exponent));
-        enPassword=hex2b64(rsaKey.encrypt(password));
-    }else{
-        enPassword='no enough params';
+    let url = ctx.url
+    let request = ctx.request
+    let req_query = request.query
+    let enPassword = null
+    let password = req_query.password
+    let exponent = req_query.exponent
+    let modules = req_query.modules
+    if (password && exponent && modules) {
+        rsaKey.setPublic(b64tohex(modules),b64tohex(exponent))
+        enPassword=hex2b64(rsaKey.encrypt(password))
+    } else {
+        enPassword = 'no enough params'
     }
-
-
     ctx.body = {
         url,
         req_query,
         enPassword
-    };
+    }
 })
-app.listen(3000);
-console.log('started');
+app.listen(3000)
+console.log('started')
